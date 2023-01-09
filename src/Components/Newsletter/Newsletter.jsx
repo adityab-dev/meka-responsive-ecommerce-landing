593bd3d401ed30fad1b9d077d07e49108e5d8cc0
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2023-01-09 16:10:25 +0530

    finished newsletter

--- src/Components/Newsletter/Newsletter.jsx
+++ src/Components/Newsletter/Newsletter.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import "./Newsletter.css";
 
 function Newsletter() {
   return (
     <section className="news-container">
-      <div className="news-left"></div>
-      <div className="news-right">
-        <h2>Join Our</h2>
-        <h2>Newsletter</h2>
-        <p>Receive exclusive deals, discounts and many offers.</p>
+      <div className="news-left-container"></div>
+      <div className="news-right-container">
+        <div className="news-right-container-center">
+          <div className="news-right">
+            <div className="news-right-upper">
+              <h2 className="news-right-heading-1">Join Our</h2>
+              <h2 className="news-right-heading-2">Newsletter</h2>
+              <p>Receive exclusive deals, discounts and many offers.</p>
+            </div>
+            <div className="news-right-input-container">
+              <input
+                type="text"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="news-right-btn-container">
+              <button
+                type="button"
+                className="news-right-btn"
+              >
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <input
-        type="text"
-        placeholder="Enter your email"
-      />
-      <button type="button">Subscribe</button>
     </section>
   );
 }
