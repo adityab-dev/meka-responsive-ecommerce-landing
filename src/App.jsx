de65eaded1ddbfe0f3ede775e66c085c3bc76f85
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2023-01-11 16:27:46 +0530

    sp.pkg last breakpoint misaligned

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ import Testimonials from "./Components/Testimonials/Testimonials";
 function App() {
   return (
     <>
-      {/* <Header /> */}
-      {/* <ExploreByCategory /> */}
+      <Header />
+      <ExploreByCategory />
       {/* <PopularProducts /> */}
       <SpecialPackage />
-      {/* <OurOwnCreation /> */}
-      {/* <Benefits /> */}
-      {/* <Testimonials /> */}
-      {/* <Newsletter /> */}
-      {/* <Footer /> */}
+      <OurOwnCreation />
+      <Benefits />
+      <Testimonials />
+      <Newsletter />
+      <Footer />
     </>
   );
 }
--- src/Components/SpecialPackage/SpecialPackage.jsx
+++ src/Components/SpecialPackage/SpecialPackage.jsx
@@ -13,20 +13,30 @@ function SpecialPackage() {
         <div className="special-container-center">
           <section className="special-content-left">
             <div className="special-big-empty"></div>
-            <div className="special-big-empty-label">Larkin Wood Full Set</div>
-            <div className="special-big-btn-container">
-              <button type="button">
-                Add to cart <AiOutlineShoppingCart />
-              </button>
+            <div className="special-big-content-container">
+              <div className="special-big-content-left">
+                <div className="special-big-empty-label">Larkin Wood Full Set</div>
+                <div className="special-5stars">
+                  <RiStarSFill />
+                  <RiStarSFill />
+                  <RiStarSFill />
+                  <RiStarSFill />
+                  <RiStarSFill />
+                </div>
+                <div className="spcial-big-empty-price">$729.99</div>
+              </div>
+
+              <div className="special-big-btn-container">
+                <button type="button">
+                  Add to cart <AiOutlineShoppingCart />
+                </button>
+              </div>
+
+              <div className="special-content-breakpoint-laptop">
+                <h3>Description</h3>
+                <p>Cast Aluminum Outdoor Chaise Lounge As an elegant and classic touch to your outdoor space, this cast...</p>
+              </div>
             </div>
-            <div className="special-5stars">
-              <RiStarSFill />
-              <RiStarSFill />
-              <RiStarSFill />
-              <RiStarSFill />
-              <RiStarSFill />
-            </div>
-            <div>$729.99</div>
           </section>
 
           <section className="special-content-right">
@@ -46,24 +56,28 @@ function SpecialPackage() {
               <div className="special-card">
                   <div className="special-small1-empty"></div>
 
-                  <div className="special-description-header-price">
-                    <div className="special-small1-card-description-top">
-                      <h2>Livin Room Special Set</h2>
-                      <div className="special-small1-price">$229.99</div>
+                  <div className="special-description-header-price special-card-background-color">
+                    <div className="special-description-header-price-center">
+                      <div className="special-small1-card-description-top">
+                        <h2>Livin Room Special Set</h2>
+                        <div className="special-small1-price">$229.99</div>
+                      </div>
+
+                      <div className="special-small1-description">
+                        <div className="special-small1-5stars">
+                          <RiStarSFill />
+                          <RiStarSFill />
+                          <RiStarSFill />
+                          <RiStarSFill />
+                          <RiStarSFill />
+                        </div>
+                        <p>
+                          See Details
+                        </p>
+                      </div>
+
                     </div>
 
-                    <div className="special-small1-description">
-                      <div className="special-small1-5stars">
-                        <RiStarSFill />
-                        <RiStarSFill />
-                        <RiStarSFill />
-                        <RiStarSFill />
-                        <RiStarSFill />
-                      </div>
-                      <p>
-                        See Details
-                      </p>
-                    </div>
                   </div>
                 </div>
 
